d5b728a1ed77f1174c67ab2fe45217f98dbb45d5
Mahpara <mah4.para@gmail.com>
2025-09-28 21:06:17 +0100

    chore: trying to fix the error

--- client/src/Components/Entries/Entries.jsx
+++ client/src/Components/Entries/Entries.jsx
@@ -16,7 +16,7 @@ export default function Entries() {
     categoryName: "",
   });
   const { type, category } = useParams();
-  const BASE_URL = import.meta.env.VITE_SERVER_BASE_URL;
+  const BASE_URL = 'https://quickkeep.onrender.com';
 
   useEffect(() => {
     async function fetchAllData() {
