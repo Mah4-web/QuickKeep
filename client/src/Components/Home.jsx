d74ae3ea23055bd3314f4921dcd8682250475f68
Mahpara <mah4.para@gmail.com>
2025-09-28 06:59:29 +0100

    chore: styled entrycard

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,14 +1,14 @@
+
 export default function Home(){
 
     return(
         <>
     <h1>Welcome to QuickKeep</h1>
     <p>Save it fast, find it faster, keep your best bits at your fingertips.</p>
+     
     </>
     );
 }
 
-
-// import "./Home.css";
 
 
